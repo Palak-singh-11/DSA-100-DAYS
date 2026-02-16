2b1de669075e8bc1902657c47d794e791d8e96ea
Palak-singh-11 <anthenasingh11@gmail.com>
2026-02-16 16:39:49 +0530

    Add recursive power function in DSA8A.c

---
+++ DSA8A.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Recursive function to calculate a^b
+long long power(int a, int b) {
+    if(b == 0)
+        return 1;
+
+    return a * power(a, b - 1);
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("%lld", power(a, b));
+
+    return 0;
+}
