6166b60546e360c4e4fd1f10ebf4e14149a95bf5
Palak-singh-11 <anthenasingh11@gmail.com>
2026-02-16 16:32:10 +0530

    Implement program to print unique elements from array

---
+++ Dsa6A.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input elements
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    if(n == 0) {
+        return 0;
+    }
+
+    int i = 0;  // Pointer for unique elements
+
+    for(int j = 1; j < n; j++) {
+        if(arr[j] != arr[i]) {
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+
+    // Print unique elements
+    for(int k = 0; k <= i; k++) {
+        printf("%d ", arr[k]);
+    }
+
+    return 0;
+}
