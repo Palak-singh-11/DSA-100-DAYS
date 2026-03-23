017a8555518a6ec989389c73e11a1484c66e5c8d
Palak-singh-11 <anthenasingh11@gmail.com>
2026-03-23 10:54:50 +0530

    git commit --date="2026-03-15T10:00:00" -m "Backdated commit"

---
+++ DAY_12A.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+
+    printf("Enter the size of square matrix: ");
+    scanf("%d", &n);
+
+    int matrix[n][n];
+
+    printf("Enter the elements of the matrix:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Check symmetry
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            if(matrix[i][j] != matrix[j][i]) {
+                printf("Matrix is not symmetric\n");
+                return 0;
+            }
+        }
+    }
+
+    printf("Matrix is symmetric\n");
+    return 0;
+}
